ecab612dec7768df0be6f98f717b03c2adfc74fd
Farhan Javid <farhanjavid@Farhans-MacBook-Air.local>
2024-09-03 21:54:54 +1000

    Initial commit with FastAPI project and Docker setup

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,25 @@ FROM python:3.12-slim
 # Set the working directory in the container
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    gcc \
+    g++ \
+    libatlas-base-dev \
+    --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copy the requirements.txt file into the container
+COPY requirements.txt /app/
+
+# Install the Python dependencies
+RUN pip install --no-cache-dir --prefer-binary -r requirements.txt
+
+# Copy the rest of the application code into the container
 COPY . /app
 
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir fastapi uvicorn
-
-# Make port 80 available to the world outside this container
+# Expose port 80 to the outside world
 EXPOSE 80
 
-# Define environment variable
-ENV NAME FastAPI_Docker
-
-# Run app.py when the container launches
+# Command to run the FastAPI application
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+import joblib
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to FastAPI!"}
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+melbourne_model = DecisionTreeRegressor(random_state=1)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+melbourne_data = pd.read_csv('data/melb_data.csv')
+X = melbourne_data[melbourne_features]
+y = melbourne_data.Price
+melbourne_model.fit(X, y)
+
+class HouseFeatures(BaseModel):
+    Rooms: int
+    Bathroom: int
+    Landsize: float
+    Lattitude: float
+    Longtitude: float
+
+
+@app.get('/')
+async def home():
+    return {
+        "Message":"Hi,This is our first ML Model."
+    }
+
+@app.post("/predict/")
+async def predict_price(features: HouseFeatures):
+    # Convert the input data to the format required by the model
+    input_data = [[
+        features.Rooms,
+        features.Bathroom,
+        features.Landsize,
+        features.Lattitude,
+        features.Longtitude
+    ]]
+
+    # Make a prediction
+    prediction = melbourne_model.predict(input_data)
+
+    # Return the prediction as a JSON response
+    return f"predicted_price: {prediction[0]:,.2f} USD"
+
+# You can run the app using the command below
+# uvicorn filename:app --reload
