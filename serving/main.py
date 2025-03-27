fc0e263a2e3f1d1fb3334557832c5d42495a86b4
Farhan Javid <farhanjavid@Farhans-MacBook-Air.local>
2025-03-27 18:20:36 +1100

    first commit

--- serving/main.py
+++ serving/main.py
@@ -20,12 +20,12 @@ app = FastAPI()
 # y = melbourne_data["Price"]
 # melbourne_model.fit(X, y)
 
-class HouseFeatures(BaseModel):
-    Rooms: int
-    Bathroom: int
-    Landsize: float
-    Lattitude: float
-    Longtitude: float
+# class HouseFeatures(BaseModel):
+#     Rooms: int
+#     Bathroom: int
+#     Landsize: float
+#     Lattitude: float
+#     Longtitude: float
 
 @app.get("/")
 async def home():
@@ -35,14 +35,14 @@ async def home():
 async def health_check():
     return {"status": "healthy"}
 
-@app.post("/predict/")
-async def predict_price(features: HouseFeatures):
-    input_data = [[
-        features.Rooms,
-        features.Bathroom,
-        features.Landsize,
-        features.Lattitude,
-        features.Longtitude
-    ]]
-    prediction = melbourne_model.predict(input_data)
-    return {"predicted_price": f"{prediction[0]:,.2f} USD"}
+# @app.post("/predict/")
+# async def predict_price(features: HouseFeatures):
+#     input_data = [[
+#         features.Rooms,
+#         features.Bathroom,
+#         features.Landsize,
+#         features.Lattitude,
+#         features.Longtitude
+#     ]]
+#     prediction = melbourne_model.predict(input_data)
+#     return {"predicted_price": f"{prediction[0]:,.2f} USD"}
